bbf03e89273555d016402c34e3be27fed18b51d4
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-04-20 22:24:00 +0300

    refactor: :construction: update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,27 @@
-import Koa, { Context } from 'koa';
+import Koa from 'koa';
+import Knex from 'knex';
 import bodyParser from 'koa-bodyparser';
+import { Model } from 'objection';
+import jwt from 'koa-jwt';
+import cors from '@koa/cors';
+
+import knexConfig from '../db/knexfile';
+import noteController from './controllers/note.controller';
+import userController from './controllers/user.controller';
+
+const knex = Knex(knexConfig.development);
+
+Model.knex(knex);
 
 const app: Koa = new Koa();
 
-app.use(async (ctx: Context, next: () => Promise<any>) => {
+app.use(bodyParser());
+app.use(cors());
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
   try {
     await next();
   } catch (error) {
+    console.log('wer are hwere', error);
     ctx.status = error.statusCode || error.status;
     error.status = ctx.status;
     ctx.body = { error };
@@ -14,14 +29,23 @@ app.use(async (ctx: Context, next: () => Promise<any>) => {
   }
 });
 
-app.use(bodyParser());
+app.use(
+  jwt({
+    secret: 'secret',
+  }).unless({
+    path: [/^\/public/, '/'],
+  }),
+);
 
-app.use(async (ctx: Context) => {
+app.use(noteController.routes());
+app.use(noteController.allowedMethods());
+app.use(userController.routes());
+app.use(userController.allowedMethods());
+app.use(async (ctx: Koa.Context) => {
   ctx.body = 'Hello world';
 });
 
 app.on('error', console.error);
-
 app.listen(3005);
 
 export default app;
