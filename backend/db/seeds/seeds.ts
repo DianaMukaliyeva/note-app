c6bcaa6eed2ec1a1705b312510b5ce83b868ef72
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-04-20 21:46:40 +0300

    update seeds

--- backend/db/seeds/seeds.ts
+++ backend/db/seeds/seeds.ts
@@ -10,4 +10,8 @@ export async function seed(knex: Knex): Promise<void> {
     { id: 1, username: 'user1', password: 'user1' },
     { id: 2, username: 'user2', password: 'user2' },
   ]);
+  await knex('notes').insert([
+    { id: 1, author_id: 1, title: 'user1 note', content: 'some test' },
+    { id: 2, author_id: 2, title: 'user2 note', content: 'some test' },
+  ]);
 }
